958ad8e6da7cda515ea90517271647bf7397163a
Kenneth Taipe <kennethtaipe@gmail.com>
2024-11-10 21:24:10 -0500

    Add project files.

---
+++ csharpbasicapp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace csharpbasicapp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
